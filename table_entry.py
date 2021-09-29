13d089978112907e8c0f6579f3737da4b30d2ce9
MojeCvicneProgramy <renatacarna@gmail.com>
2021-09-29 12:25:32 +0200

    Základná verzia

---
+++ table_entry.py
@@ -0,0 +1,23 @@
+import PySimpleGUI as sg
+
+# výber farby formulára:
+sg.theme('DarkTeal9')
+
+# rozloženie formulára:
+layout = [
+	[sg.Text('Vyplňte prosím nasledujúce polia: ')],
+	[sg.Text('Meno', size=(15,1)), sg.InputText(key='Meno')],
+	[sg.Submit('Uložiť'), sg.Exit('Ukončiť')]
+]
+
+# vytvorenie okna, v ktorom sa to bude zobrazovať:
+okno = sg.Window('Jednoduchý formulár pre vkladanie údajov', layout)
+
+#
+while True:
+	event, values = okno.read()
+	if event == sg.WIN.CLOSED or event == 'Exit':
+		break
+	if event == 'Submit':
+		print(event, values)
+okno.close()
